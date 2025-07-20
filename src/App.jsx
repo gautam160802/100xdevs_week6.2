794f95c58d57a67def58c354a320b0c8e2f223ef
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-20 20:39:13 +0530

    adding usememo

--- src/App.jsx
+++ src/App.jsx
@@ -33,58 +33,98 @@
 
 // assingment problem
 
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [selectedId, setSelectedId] = useState(1);
+//   return (
+//     <div>
+//       <button
+//         onClick={function () {
+//           setSelectedId(1);
+//         }}
+//       >
+//         1
+//       </button>
+//       <button
+//         onClick={function () {
+//           setSelectedId(2);
+//         }}
+//       >
+//         2
+//       </button>
+//       <button
+//         onClick={function () {
+//           setSelectedId(3);
+//         }}
+//       >
+//         3
+//       </button>
+
+//       <Todo id={selectedId} />
+//     </div>
+//   );
+// }
+
+// function Todo({ id }) {
+//   const [todo, setTodo] = useState({});
+
+//   useEffect(() => {
+//     fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setTodo(data); // ✅ corrected here
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching todo:", error);
+//       });
+//   }, [id]);
+
+//   return (
+//     <div>
+//       Id: {id}
+//       <h1>{todo.title}</h1>
+//       <h4>{`Completed: ${todo.completed}`}</h4>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// use memo
+import { useState, useMemo } from "react";
 
 function App() {
-  const [selectedId, setSelectedId] = useState(1);
-  return (
-    <div>
-      <button
-        onClick={function () {
-          setSelectedId(1);
-        }}
-      >
-        1
-      </button>
-      <button
-        onClick={function () {
-          setSelectedId(2);
-        }}
-      >
-        2
-      </button>
-      <button
-        onClick={function () {
-          setSelectedId(3);
-        }}
-      >
-        3
-      </button>
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
 
-      <Todo id={selectedId} />
-    </div>
-  );
-}
+  const count = useMemo(() => {
+    let finalCount = 0;
 
-function Todo({ id }) {
-  const [todo, setTodo] = useState({});
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setTodo(data); // ✅ corrected here
-      })
-      .catch((error) => {
-        console.error("Error fetching todo:", error);
-      });
-  }, [id]);
+    for (let i = 0; i <= inputValue; i++) {
+      finalCount = finalCount + i;
+    }
+    return finalCount;
+  }, [inputValue]);
 
   return (
     <div>
-      Id: {id}
-      <h1>{todo.title}</h1>
-      <h4>{`Completed: ${todo.completed}`}</h4>
+      <input
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+        placeholder={"Find sum from 1 to n"}
+      ></input>
+      <br />
+      Sum from 1 to {inputValue} is {count}
+      <br />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter ({counter})
+      </button>
     </div>
   );
 }
