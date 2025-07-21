8e5c3e4b0d2ab7be574851a7a00780f80a509ba2
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-07-21 19:43:37 +0530

    adding callback

--- src/App.jsx
+++ src/App.jsx
@@ -91,42 +91,79 @@
 
 // export default App;
 
-// use memo
-import { useState, useMemo } from "react";
+// // use memo
+// import { useState, useMemo } from "react";
+
+// function App() {
+//   const [counter, setCounter] = useState(0);
+//   const [inputValue, setInputValue] = useState(1);
+
+//   const count = useMemo(() => {
+//     let finalCount = 0;
+
+//     for (let i = 0; i <= inputValue; i++) {
+//       finalCount = finalCount + i;
+//     }
+//     return finalCount;
+//   }, [inputValue]);
+
+//   return (
+//     <div>
+//       <input
+//         onChange={function (e) {
+//           setInputValue(e.target.value);
+//         }}
+//         placeholder={"Find sum from 1 to n"}
+//       ></input>
+//       <br />
+//       Sum from 1 to {inputValue} is {count}
+//       <br />
+//       <button
+//         onClick={() => {
+//           setCounter(counter + 1);
+//         }}
+//       >
+//         Counter ({counter})
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// use callback
+
+import { memo, useCallback, useState } from "react";
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [inputValue, setInputValue] = useState(1);
+  const [count, setCount] = useState(0);
 
-  const count = useMemo(() => {
-    let finalCount = 0;
-
-    for (let i = 0; i <= inputValue; i++) {
-      finalCount = finalCount + i;
-    }
-    return finalCount;
-  }, [inputValue]);
+  const inputFuntion = useCallback(() => {
+    console.log("Input function called");
+  }, []);
 
   return (
     <div>
-      <input
-        onChange={function (e) {
-          setInputValue(e.target.value);
-        }}
-        placeholder={"Find sum from 1 to n"}
-      ></input>
-      <br />
-      Sum from 1 to {inputValue} is {count}
-      <br />
+      <ButtonComponent inputFuntion={inputFuntion} />
       <button
         onClick={() => {
-          setCounter(counter + 1);
+          setCount(count + 1);
         }}
       >
-        Counter ({counter})
+        Click me {count}
       </button>
     </div>
   );
 }
+
+const ButtonComponent = memo(({ inputFuntion }) => {
+  console.log("chiled render");
+
+  return (
+    <div>
+      <button>Button clicked</button>
+    </div>
+  );
+});
 
 export default App;
